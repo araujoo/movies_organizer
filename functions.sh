1c9e3134ae1e52f2a059e67c684eab9a84c5f4f4
Oberdan Araujo <oberdan_araujo@id.uff.br>
2018-03-12 02:24:44 -0300

    release 1.0

--- functions.sh
+++ functions.sh
@@ -1,13 +1,13 @@
 get_new_file_name()
 {
 	local NEW_FILE_NAME RC_CODE
-	
+	#echo "entrei aqui com ${1}"
 	#test wich regex applies to the given filename
 	for i in ${!FILE_REGEXS[@]}
 	do
 		#echo "testing ${1} with ${FILE_REGEXS[i]}"
 		NEW_FILE_NAME=''
-	    NEW_FILE_NAME="$(echo ${1} | egrep -o ${FILE_REGEXS[i]} )"
+	    NEW_FILE_NAME="$(echo ${1} | egrep -o ${SPECIAL_CHARS_REGEX}${FILE_REGEXS[$i]}${SPECIAL_CHARS_REGEX}* )"
 		if [ ! -z "${NEW_FILE_NAME}" ]
 		then
 			break
@@ -16,6 +16,9 @@ get_new_file_name()
 
 	if [ ! -z "${NEW_FILE_NAME}" ]
 	then
+
+		#cleans the filename
+		NEW_FILE_NAME="$( echo ${NEW_FILE_NAME} | sed "s:${SPECIAL_CHARS_REGEX}::g" )"
 		case ${i} in
 			0)
 				NEW_FILE_NAME="$( echo ${NEW_FILE_NAME} | tr '[:lower:]' '[:upper:]' )"
@@ -25,7 +28,17 @@ get_new_file_name()
 				NEW_FILE_NAME="$(echo "S0${NEW_FILE_NAME:0:1}E${NEW_FILE_NAME:1:2}" )"
 				RC_CODE=0
 				;;
-			2)
+			2)  
+				NEW_FILE_NAME="$(echo "S0${NEW_FILE_NAME:0:1}E${NEW_FILE_NAME:2:2}" )"
+				RC_CODE=0
+				;;
+				
+			3)  
+				NEW_FILE_NAME="$(echo "S0${NEW_FILE_NAME:0:2}E${NEW_FILE_NAME:2:2}" )"
+				RC_CODE=0
+				;;
+				
+			*)
 				NEW_FILE_NAME="Failed to identify pattern. Please, add a new one."
 				RC_CODE=1
 				;;
@@ -37,15 +50,14 @@ get_new_file_name()
 
 extract_file()
 {
-	echo "extract file estou em $PWD"
-	local extens="$( get_file_extension ${1} )"
-	echo "extens = ${extens}"
-	case "${extens}" in
-		".rar")	echo "entrei no caso .rar"
-				unrar e "${1}"
+	local EXTENS
+	#echo "extract file"
+	EXTENS="$( get_file_extension ${1} )"
+	#echo "extens = ${EXTENS}"
+	case "${EXTENS}" in
+		".rar")	unrar e "${1}" > '/dev/null'
 				;;
-		".zip")	echo "entrei no caso .zip"
-				unzip "${1}"
+		".zip")	unzip "${1}" > '/dev/null'
 				;;
 	esac
 }
@@ -57,21 +69,20 @@ get_file_extension()
 
 check_file_in_list()
 {
-	local RC_CODE
+	local RC_CODE IFS
 
-	#set IFS for comparing
+	#set IFS for comparison
 	IFS=':'
 
 	#fetch
 	for f in ${2}
 	do
 		#echo "comparing ${f} with ${1}"
-		if [ "${f}" == "${1}" -a -z "${f}" ]
+		if [ "${f}" == "${1}" ] && [ ! -z "${f}" ]
 		then
 			RC_CODE=0;
+			break
 		fi
 	done
-	RC_CODE=1
-	unset IFS
-	return ${RC_CODE}
+	return ${RC_CODE=1}
 }
--- macros.sh
+++ macros.sh
@@ -1,6 +1,6 @@
 FILES_NUM=0
-FILE_REGEXS=( '[sS][0-9]{2}[eE][0-9]{2}' '[0-9]{3}' )
-EXTENSION_REGEX='\.[a-z]*$'
+FILE_REGEXS=( '[sS][0-9]{2}[eE][0-9]{2}' '[0-9]{3}' '[0-9]x[0-9]{2}' '[0-9]{2}x[0-9]{2}' )
+EXTENSION_REGEX='\.[A-Za-z0-9]*$'
 COMPACT_EXTENSIONS=( ".rar" ".zip" )
 SUB_EXTENSIONS='.srt'
 MOV_EXTENSIONS='.mkv .mp4 .avi'
@@ -8,3 +8,4 @@ SUBS_BKP_DIR='Subs'
 TORRENTS_BKP_DIR='Torrents'
 SOURCES_BKP_DIR='Sources'
 FILE_LIST=""
+SPECIAL_CHARS_REGEX='[^A-Za-z0-9]'
